b175c4d5dc94355a850bf2f5d683857c6cc7066a
William Tarte <william.tarte6@gmail.com>
2020-06-28 15:40:11 -0400

    Created CMakeLists and dummy program. Linkage errors with GLEW

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # COMP-371-Proj
+
+## Sources Versions
+ - glfw-3.3.2
+ - glew-2.1.0
+ - glm-0.9.9.8
+ 
+## Resources
+ - https://learnopengl.com/
+ - I copied the CMakeLists.txt from here: https://github.com/andersonfreitas/opengl-boilerplate/blob/master/CMakeLists.txt
---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    int i;
+    std::cin >> i;
+    return 0;
+}
+
+
+
