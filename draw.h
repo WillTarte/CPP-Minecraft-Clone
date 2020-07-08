b56d833375572537c74d3ed141c5d764cadcb9b8
Zahra Nikbakht <60522280+zahranikbakht@users.noreply.github.com>
2020-07-08 01:13:54 -0400

    Added H3

--- draw.h
+++ draw.h
@@ -171,4 +171,126 @@ public:
     }
 };
 
+
+class H3
+{
+
+private:
+    GLuint vbo{}, vao{}, ebo{};
+    GLsizei size{};
+    Shader shader;
+    std::vector<glm::vec3> vertices;
+    const GLushort indices[36] = {
+        //front
+                   0, 1, 3, 
+                   2, 3, 1,
+                   //back
+                   4, 7, 5, 
+                   6, 5, 7,
+                   //left
+                   4, 0, 7, 
+                   3, 7, 0,
+                   //right
+                   5, 6, 1, 
+                   2, 1, 6,
+                   //bottom
+                   0, 4, 1, 
+                   5, 1, 4,
+                   //top
+                   3, 2, 7, 
+                   6, 7, 2
+    };
+
+public:
+
+    H3() {
+
+
+        this->shader = Shader("resources/shaders/ModelVertexShader.vs", "resources/shaders/ModelFragmentShader.fs");
+        vertices.emplace_back(glm::vec3(-0.5, -0.5, 0.5));
+        vertices.emplace_back(glm::vec3(0.5, -0.5, 0.5));
+        vertices.emplace_back(glm::vec3(0.5, 0.5, 0.5));
+        vertices.emplace_back(glm::vec3(-0.5, 0.5, 0.5));
+        vertices.emplace_back(glm::vec3(-0.5, -0.5, -0.5));
+        vertices.emplace_back(glm::vec3(0.5, -0.5, -0.5));
+        vertices.emplace_back(glm::vec3(0.5, 0.5, -0.5));
+        vertices.emplace_back(glm::vec3(-0.5, 0.5, -0.5));
+
+
+        this->size = sizeof(indices) / sizeof(unsigned short);
+
+        glGenVertexArrays(1, &vao);
+        glGenBuffers(1, &vbo);
+        glGenBuffers(1, &ebo);
+
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), vertices.data(), GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, sizeof(glm::vec3), nullptr);
+        glEnableVertexAttribArray(0);
+    }
+
+
+
+
+
+    //unit cube
+    void DrawCube(glm::mat4 base, glm::mat4 transform)
+    {
+        shader.use();
+        shader.setMat4("base_mvp", base);
+        shader.setMat4("transform", transform);
+
+        glBindVertexArray(vao);
+        
+        glDrawElements(GL_TRIANGLES, size, GL_UNSIGNED_SHORT, nullptr);
+
+
+    }
+    void DrawH3(glm::mat4 base) {
+
+        //draw H
+        glm::mat4 scalingMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 3.0f, 1.0f));
+        glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 1.5f, 0.0f));
+        glm::mat4 rotationMatrix = glm::mat4(1.0f);
+        DrawCube(base, translationMatrix * rotationMatrix * scalingMatrix);
+
+        scalingMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 1.5f, 0.0f));
+        rotationMatrix = glm::mat4(1.0f);
+        DrawCube(base, translationMatrix * rotationMatrix * scalingMatrix);
+
+        scalingMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 3.0f, 1.0f));
+        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(2.0f, 1.5f, 0.0f));
+        rotationMatrix = glm::mat4(1.0f);
+        DrawCube(base, translationMatrix * rotationMatrix * scalingMatrix);
+
+        //draw 3
+        scalingMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f, 3.0f, 1.0f));
+        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(4.5f, 1.5f, 0.0f));
+        rotationMatrix = glm::mat4(1.0f);
+        DrawCube(base, translationMatrix * rotationMatrix * scalingMatrix);
+
+        scalingMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 0.5f, 1.0f));
+        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(4.0f, 0.25f, 0.0f));
+        rotationMatrix = glm::mat4(1.0f);
+        DrawCube(base, translationMatrix * rotationMatrix * scalingMatrix);
+
+
+        scalingMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 0.5f, 1.0f));
+        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(4.0f, 2.75f, 0.0f));
+        rotationMatrix = glm::mat4(1.0f);
+        DrawCube(base, translationMatrix * rotationMatrix * scalingMatrix);
+
+        scalingMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 0.5f, 1.0f));
+        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(4.0f, 1.5f, 0.0f));
+        rotationMatrix = glm::mat4(1.0f);
+        DrawCube(base, translationMatrix * rotationMatrix * scalingMatrix);
+
+    }
+};
+
 #endif //COMP_371_PROJECT_DRAW_H
