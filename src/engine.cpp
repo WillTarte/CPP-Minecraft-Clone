aed8c16574d4b02169ea39428388d30bb7e78603
Zahra <zahraa_nik@yahoo.com>
2020-08-19 01:08:02 -0400

    simplified the randomization algorithm

--- src/engine.cpp
+++ src/engine.cpp
@@ -250,14 +250,13 @@ void Engine::generateWorld() {
         while (true) {
             int x = (rand() % 32) + WORLD_WIDTH / 2;
             int z = (rand() % 32) + WORLD_LENGTH / 2;
-            int height[6];
-            for (unsigned int i = 0; i < 6; i++) {
+            int height[2];
+            for (unsigned int i = 0; i < 2; i++) {
                 float tempHeight = noiseGen.GetNoise(x + i, 0, z) + 1;
                 height[i] = static_cast<int>(round((tempHeight * 10.0f) + 1.0f) + 10.0f);
 
             }
-            if (height[0] >= 14 && height[1] >= 14 && height[2] >= 14 && height[3] >= 14 && height[4] >= 14 &&
-                height[5] == height[0]) {
+            if (height[0] >= 14 && height[1] == height[0]) {
                 if(j==0) {
                     addA2(x, height[0], z);
                 } else if(j==1){
@@ -270,7 +269,7 @@ void Engine::generateWorld() {
                 } else if(j==4) {
                     addH3(x, height[0], z);
                 }
-                LOG(INFO) << "this is x" << x << "this is z " << z << "}";
+
 
                 break;
             }
