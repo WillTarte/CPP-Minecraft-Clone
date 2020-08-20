ef9cf8c2f8042ee05962bacf24eac66635379b5a
WillTarte <william.tarte6@gmail.com>
2020-08-19 21:42:17 -0400

    irrkllang on windows/msvc

--- main.cpp
+++ main.cpp
@@ -2,10 +2,23 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <iostream>
+#include <filesystem>
+#include "libs/irrKLang/include/irrKlang.h"
 #include "libs/easylogging++.h"
 #include "include/engine.h"
 
 INITIALIZE_EASYLOGGINGPP
+
+#ifdef __unix__
+namespace fs = std::filesystem;
+#endif
+
+#if defined __unix__ || _MSC_VER >= 1914
+namespace fs = std::filesystem;
+#else
+namespace fs = std::filesystem;
+#endif
 
 void initLogging();
 
@@ -13,6 +26,16 @@ void initLogging();
 int main(int argc, char *argv[]) {
 
     initLogging();
+
+
+    irrklang::ISoundEngine *soundEngine = irrklang::createIrrKlangDevice();
+
+    if (!soundEngine) {
+        LOG(ERROR) << "Could not create sound device";
+        return -1;
+    }
+
+    soundEngine->play2D((fs::current_path().string() + "./resources/sounds/calm.mp3").c_str(), true);
 
     auto engine = Engine({});
 
